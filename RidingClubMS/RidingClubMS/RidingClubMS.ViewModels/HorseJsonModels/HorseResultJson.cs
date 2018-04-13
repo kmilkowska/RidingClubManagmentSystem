2df52cd5d5a09a1b90572c9b00697dae640b138f
kmilkowska <kmilkowska@ada23c3a-ab84-b49c-917a-ce57b7db7bff>
2018-04-13 09:47:36 +0000

    add
    git-svn-id: https://github.com/kmilkowska/RidingClubManagmentSystem.git/trunk@12 ada23c3a-ab84-b49c-917a-ce57b7db7bff

---
+++ RidingClubMS/RidingClubMS/RidingClubMS.ViewModels/HorseJsonModels/HorseResultJson.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RidingClubMS.ViewModels.HorseJsonModels
+{
+    public class HorseResultJson
+    {
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public string BirthDay { get; set; }
+        public string Breeder { get; set; }
+    }
+}
